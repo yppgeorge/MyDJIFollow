28de6d267ed91e77c40e239f31643fa0df4ae1cc
Praveen George Yagappa <38196425+yppgeorge@users.noreply.github.com>
2021-09-07 13:28:09 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from djitellopy import Tello
+import cv2
+import time
+
+width = 320
+height = 240
+startCounter = 0
+
+me = Tello()
+me.connect()
+
+me.for_back_velocity = 0
+me.for_right_velocity = 0
+me.up_down_velocity = 0
+me.speed = 0
+me.yaw_velocity = 0
+
+print(me.get_battery())
+
+if startCounter == 0:
+    me.takeoff()
+    me.get_height()
+    me.land()
