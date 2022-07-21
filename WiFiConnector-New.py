d1b5461feeaeb921085ca9fc411cc7107448d483
Praveen George Yagappa <38196425+yppgeorge@users.noreply.github.com>
2022-07-21 12:50:09 -0600

    updating additional programs

--- WiFiConnector-New.py
+++ WiFiConnector-New.py
@@ -5,6 +5,46 @@
 # import module
 import os
 
+def createNewConnection(name, SSID, password):
+    config = """<?xml version=\"1.0\"?>
+    <WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
+        <name>"""+name+"""</name>
+        <SSIDConfig>
+            <SSID>
+                <name>"""+SSID+"""</name>
+            </SSID>
+        </SSIDConfig>
+        <connectionType>ESS</connectionType>
+        <connectionMode>auto</connectionMode>
+        <MSM>
+            <security>
+                <authEncryption>
+                    <authentication>WPA2PSK</authentication>
+                    <encryption>AES</encryption>
+                    <useOneX>false</useOneX>
+                </authEncryption>
+                <sharedKey>
+                    <keyType>passPhrase</keyType>
+                    <protected>false</protected>
+                    <keyMaterial>"""+password+"""</keyMaterial>
+                </sharedKey>
+            </security>
+        </MSM>
+    </WLANProfile>"""
+    command = "netsh wlan add profile filename=\"" + name + ".xml\"" + " interface=Wi-Fi"
+    with open(name + ".xml", 'w') as file:
+        file.write(config)
+    os.system(command)
+
+# function to connect to a network
+def connect(name, SSID):
+    command = "netsh wlan connect name=\""+name+"\" ssid=\""+SSID+"\" interface=Wi-Fi"
+    os.system(command)
+
+# function to display avavilabe Wifi networks
+def displayAvailableNetworks():
+    command = "netsh wlan show networks interface=Wi-Fi"
+    os.system(command)
 
 # display available netwroks
 displayAvailableNetworks()
